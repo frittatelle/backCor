c32413df877813d3fa94b4d90f620f97a0381b58
Luca Ferrari <myfavouritefreakelle@gmail.com>
2019-10-05 12:46:12 +0200

    /fonts dir

--- backCor/installBackCor.bat
+++ backCor/installBackCor.bat
@@ -5,5 +5,7 @@ pyinstaller --log-level=DEBUG ^
     --add-data="C:\Users\Luca\github\backCor\data\userData\settings.json;data\userData" ^
     --add-data="C:\Users\Luca\github\backCor\img\ico\backCor_BB.ico;img\ico" ^
     --add-data="C:\Users\Luca\github\backCor\img\ico\backCor_WB.ico;img\ico" ^
+    --add-binary="C:\Users\Luca\github\backCor\fonts\Poppins-Regular.ttf;fonts" ^
+    --add-binary="C:\Users\Luca\github\backCor\fonts\Poppins-Bold.ttf;fonts" ^
     --icon="C:\Users\Luca\github\backCor\img\ico\backCor_BB.ico" ^
     backCor.py
