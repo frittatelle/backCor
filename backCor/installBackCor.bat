c36c69ec394dacb9c2b60a9d599016b881521c9e
Luca Ferrari <myfavouritefreakelle@gmail.com>
2019-10-26 17:15:52 +0200

    Edit - Diffrent directories

--- backCor/installBackCor.bat
+++ backCor/installBackCor.bat
@@ -1,13 +1,13 @@
 pyinstaller --log-level=DEBUG ^
     --noconsole ^
-    --add-data="C:\Users\Luca\github\backCor\data\sampleData\sample_1_multi.wdf;data\sampleData" ^
-    --add-data="C:\Users\Luca\github\backCor\data\sampleData\sample_2_single.wdf;data\sampleData" ^
-    --add-data="C:\Users\Luca\github\backCor\data\sampleData\sample_3_multi.txt;data\sampleData" ^
-    --add-data="C:\Users\Luca\github\backCor\data\sampleData\sample_4_single.txt;data\sampleData" ^
-    --add-data="C:\Users\Luca\github\backCor\data\userData\settings.json;data\userData" ^
-    --add-data="C:\Users\Luca\github\backCor\img\ico\backCor_BB.ico;img\ico" ^
-    --add-data="C:\Users\Luca\github\backCor\img\ico\backCor_WB.ico;img\ico" ^
-    --add-binary="C:\Users\Luca\github\backCor\fonts\Poppins-Regular.ttf;fonts" ^
-    --add-binary="C:\Users\Luca\github\backCor\fonts\Poppins-Bold.ttf;fonts" ^
-    --icon="C:\Users\Luca\github\backCor\img\ico\backCor_BB.ico" ^
+    --add-data="C:\Users\Luca\github\backCor\backCor\data\sampleData\sample_1_multi.wdf;data\sampleData" ^
+    --add-data="C:\Users\Luca\github\backCor\backCor\data\sampleData\sample_2_single.wdf;data\sampleData" ^
+    --add-data="C:\Users\Luca\github\backCor\backCor\data\sampleData\sample_3_multi.txt;data\sampleData" ^
+    --add-data="C:\Users\Luca\github\backCor\backCor\data\sampleData\sample_4_single.txt;data\sampleData" ^
+    --add-data="C:\Users\Luca\github\backCor\backCor\data\userData\settings.json;data\userData" ^
+    --add-data="C:\Users\Luca\github\backCor\backCor\img\ico\backCor_BB.ico;img\ico" ^
+    --add-data="C:\Users\Luca\github\backCor\backCor\img\ico\backCor_WB.ico;img\ico" ^
+    --add-binary="C:\Users\Luca\github\backCor\backCor\fonts\Poppins-Regular.ttf;fonts" ^
+    --add-binary="C:\Users\Luca\github\backCor\backCor\fonts\Poppins-Bold.ttf;fonts" ^
+    --icon="C:\Users\Luca\github\backCor\backCor\img\ico\backCor_BB.ico" ^
     backCor.py
